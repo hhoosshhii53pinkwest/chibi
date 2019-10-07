9042b7e43e9e48f4eee38903fb319d8568317531
FUJIMAKI Seika <21716080@jwu.ac.jp>
2019-10-07 16:47:03 +0900

    Q class

--- rational.py
+++ rational.py
@@ -1,7 +1,7 @@
 import math
 class Q(object):
     def __init__(self, a, b=1): #コントラスタ
-        gcd = math.gcd(a,b)
+       
         self.a = a
         self.b = b
     def __repr__(self):
@@ -14,24 +14,24 @@ class Q(object):
         c = q.a
         d = q.b
         return Q(a*d+b*c, b*d)
-        def __sub__(self, q):
+    def __sub__(self, q):
         a = self.a
         b = self.b
         c = q.a
         d = q.b
         return Q(a*d-b*c, b*d)
-        def __mul__(self, q):
+    def __mul__(self, q):
         a = self.a
         b = self.b
         c = q.a
         d = q.b
-        return Q(a*c/b*d)
-        def __truediv__(self, q):
+        return Q(a*c,b*d)
+    def __truediv__(self, q):
         a = self.a
         b = self.b
         c = q.a
         d = q.b
-        return Q(a*d/b*c)
+        return Q(a*d,b*c)
 
 q1 = Q(1,2)
 q2 = Q(1,3)
