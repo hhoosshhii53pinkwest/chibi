c7257452549bcf914ca899c1a98fd2bd85c45951
FUJIMAKI Seika <21716080@jwu.ac.jp>
2019-10-14 16:19:51 +0900

    Expr

--- exp.py
+++ exp.py
@@ -1,4 +1,5 @@
-
+class Expr(object):
+    pass
 class Val(object):
     __slots__ = ['value']
     def __init__(self,value= 0):
@@ -12,6 +13,9 @@ v = Val(1)
 print(v)
 assert v.eval() == 1
 
+assert isinstance(v, Expr) #==>True
+assert isinstance(v, Val) #==>True
+assert isinstance(v, int) #==>False
 class Add(object):
     __slots__=['left','right']
     def __init__(self, a, b):
@@ -23,6 +27,10 @@ class Add(object):
 e = Add(Val(1), Val(2)) #1+2
 print(e.eval())
 assert e.eval() == 3
+
+e = Add(1,2)
+assert e.eval() == 3
+
 
 e = Add(Val(1),Add(Val(2),Val(3))) #1+2+3 ==> 6
 print(e.eval())
@@ -74,3 +82,4 @@ print(e.eval())
 assert e.eval() == 3
 
 print()
+
